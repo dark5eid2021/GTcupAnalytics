ae166daffda96fad0674cf38221c9aab5832cf15
Jason Boykin <jasonboykin@gmail.com>
2025-02-20 10:03:49 -0600

    fixed spelling errors

--- train_model.py
+++ train_model.py
@@ -12,7 +12,7 @@ df = pd.read_csv("porsche_data.csv")
 X = df[["speed", "engine_temp"]]
 y = df[["fuel_efficiency"]]
 
-# split data innto training and test sets
+# split data into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 
